5ece38623535f48c440812b9f84998e960e5f18f
unknown <stud2015@ws-2k-110-n02.ccam.cmc.unn.net>
2015-11-06 15:45:56 +0300

    Popravvil struct

--- CNode/Del.h
+++ CNode/Del.h
@@ -1,17 +1,17 @@
 #include "CNode.h"
 
-void Del(CNode** first)
+void Del(struct CNode** first)
 {
 	if (*first!=0)
 	{
-		CNode* curr=*first;
+		struct CNode* curr=*first;
 		while (curr!=0)
 		{
 			if (curr->data != (curr->next)->data)
 				curr=curr->next;
 			else
 			{
-				CNode* tmp;
+				struct CNode* tmp;
 				tmp=curr->next;
 				curr->next=(curr->next)->next;
 				delete tmp;
